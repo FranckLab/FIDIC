827d5dd64b008d9a5876647f4779fbb10addf75e
Franck Lab <FranckLab@users.noreply.github.com>
2016-06-24 15:33:07 -0400

    Update checkConvergenceSSD_2D.m

--- checkConvergenceSSD_2D.m
+++ checkConvergenceSSD_2D.m
@@ -2,9 +2,9 @@ function [converged01, SSE1 , sSize1, sSpacing1] = checkConvergenceSSD_2D(I,SSE,
 % [converged01, SSD1 , sSize1, sSpacing1] =
 % checkConvergenceSSD(I,SSD,sSize,sSpacing,convergenceCrit) checks the
 % convergence of the IDVC. The convergence is based on the sum of squared
-% error (SSE) similarity metric between the undeformed and deformed 
-% image. 
-% 
+% error (SSE) similarity metric between the undeformed and deformed
+% image.
+%
 % INPUTS
 % -------------------------------------------------------------------------
 %   I: cell containing the undeformed, I{1}, and deformed, I{2} 3-D images
@@ -22,17 +22,17 @@ function [converged01, SSE1 , sSize1, sSpacing1] = checkConvergenceSSD_2D(I,SSE,
 %                vice versa
 %   SSE1: SSE for current iteration
 %   sSize1: interrogation window (subset) size for the current iteration
-%   sSpacing1: interrogation window (subset) spacing for the current 
+%   sSpacing1: interrogation window (subset) spacing for the current
 %              iteration
-%   
+%
 % NOTES
 % -------------------------------------------------------------------------
 % You are welcome to change the convergence method however you'd like. The
 % default constants are based on empirical results.
-% 
+%
 % If used please cite:
-% Bar-Kochba E., Toyjanova J., Andrews E., Kim K., Franck C. (2014) A fast 
-% iterative digital volume correlation algorithm for large deformations. 
+% Bar-Kochba E., Toyjanova J., Andrews E., Kim K., Franck C. (2014) A fast
+% iterative digital volume correlation algorithm for large deformations.
 % Experimental Mechanics. doi: 10.1007/s11340-014-9874-2
 
 I{1}(isnan(I{1})) = 0;
@@ -61,30 +61,30 @@ if iteration > 1 % skip before first displacement estimation
     sSize1 = sSize0/2; % window size refinement
     
     % ensure that all subset sizes are at minimum 32 pixels in length
-    sSize1(sSize1 < 16) = 16; 
+    sSize1(sSize1 < 16) = 16;
     
     % window spacing refinement. Only do if the sSpacing > 8 pixels
     if (sSpacing0 > 8), sSpacing1 = sSize1/2;
-             
-
-    end 
+        
+        
+    end
     
     if prod(single(sSpacing1 == 16)) % condition if spacing = 16
-      
+        
         idx = (find(prod(single(sSpacing == 16),2))-1):iteration;
         if length(idx) > 2
             dSSE = diff(SSE(idx)); % calculate difference
             dSSE = dSSE/dSSE(1); % normalize difference
-             
+            
             % if dSSE meets first convergence criteria then refine spacing
             % to the minimum value, 8 voxels.
             if dSSE(end) <= convergenceCrit(1)
-                sSize1 = sSize0; sSpacing1 = [8 8]; 
+                sSize1 = sSize0; sSpacing1 = [8 8];
                 
             end
         end
         
-    % condition if spacing is the minimum, 8 voxels
+        % condition if spacing is the minimum, 8 voxels
     elseif  prod(single(sSpacing1 == 8))
         idx = (find(prod(single(sSpacing == 8),2))-1):iteration;
         
@@ -95,7 +95,7 @@ if iteration > 1 % skip before first displacement estimation
             % if dSSE meets first convergence criteria and spacing is the
             % mimumum then convergence has been met and stop all
             % iterations.
-            if dSSE(end) <= convergenceCrit(2) 
+            if dSSE(end) <= convergenceCrit(2)
                 sSize1 = sSize0; sSpacing1 = sSpacing0;
                 converged01 = 1;
             end
